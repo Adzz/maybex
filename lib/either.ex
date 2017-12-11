18ead44dbe8b5cb8fe9643d83f8cd498a2b411d9
Adam Lancaster <lancaster28@hotmail.com>
2017-12-11 07:33:02 +0000

    adds some module docs

--- lib/either.ex
+++ lib/either.ex
@@ -1,20 +1,28 @@
 defmodule Either do
   @moduledoc """
-  This is what you use to wrap a value that might return nil.
+  This is what you use to wrap a value that might be nil.
   """
 
   @doc """
   When a value is wrapped by this function it will return either a `Right` if the value is not
   nil, or a `Left` if the value passed in is nil. The `Left` implements the map and fold functions
-  differently from the `Right`. When we eventually fold the data out, we will be returned the result
-  of the left_function if the thing being passed into the fold is a `Left`, and we will be returned
-  the result of the right_function if the thing being passed into the fold is a `Right`.
+  differently from the `Right`. This means you can pipe the result directly into a chain of Maybe
+  function.
 
   Effectively this allows stuff like this to happen:
 
   ```
-  Either.new(function_that_might_return_nil("argument"))
-  |> Maybe.fold(fn(x) -> "Whoops!" end, fn(x)-> x end)
+  iex> Either.new(nil)
+  ...> %Left{value nil}
+
+  iex> Either.new(10)
+  ...> %Rigt{value: 10}
+
+  iex> Either.new(nil) |> Maybe.map(fn(_) "this wont run" end)
+  ...> %Left{value: nil}
+
+  iex> Either.new(10) |> Maybe.map(fn(x) x + 10 end)
+  ...> %Right{value: 20}
   """
   def new(value) do
     if value do
