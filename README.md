9397cd54d350bf938011d7bb30aa61babdd5dd55
Adam Lancaster <lancaster28@hotmail.com>
2017-12-22 18:30:37 +0000

    adds ExDoc as a dep

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Maybe
 
-**TODO: Add description**
+To run test at the root of the project run `mix test`
 
 ## Installation
 
@@ -18,4 +18,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/maybe](https://hexdocs.pm/maybe).
-
--- mix.exs
+++ mix.exs
@@ -1,28 +1,32 @@
-defmodule Maybe.Mixfile do
+defmodule Maybex.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :maybe,
-      version: "0.1.0",
+      app: :maybex,
+      version: "0.0.1",
       elixir: "~> 1.5",
+      # Docs
+      name: "Maybex",
+      source_url: "https://github.com/adzz/maybex",
+      docs: [
+        main: "Either",
+        extras: ["README.md"]
+        ],
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 end
