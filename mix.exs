3bcb903f19fc049f83f881c69188402b659b680a
Adam Lancaster <lancaster28@hotmail.com>
2018-01-14 17:37:30 +0000

    bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Maybex.Mixfile do
   def project do
     [
       app: :maybex,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.5",
       # Docs
       name: "Maybex",
